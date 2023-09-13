4cc978d5f4dde3a042e6f2bea2828733b763ec23
Andrew Byun <andrewbyun1@gmail.com>
2023-09-12 21:24:02 -0700

    final push for day1

--- src/index.js
+++ src/index.js
@@ -1,1 +1,6 @@
-console.log('hello')
+import React from 'react';
+import { render } from 'react-dom';
+import './style.css'
+console.log('WHAT IT DO DREW')
+
+
--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,38 @@
 const path = require('path');
-
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 module.exports = {
-    entry: './src/index.js',
-    mode: 'development',
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'bundle.js'
-    }
-}
+  entry: './src/index.js',
+  mode: 'development',
+  devServer: {
+    open: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.jsx?/, // Match both .js and .jsx files
+        exclude: /node_modules/, // Don't transpile files in node_modules
+        use: {
+          loader: 'babel-loader', // Use babel-loader for transpilation
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react'],
+          },
+        },
+      },
+    ],
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      template: 'src/index.html',
+    }),
+  ],
+};
